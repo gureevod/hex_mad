8a37b909306b0014824ce47baff71db41614177a
gureevod <74977527+gureevod@users.noreply.github.com>
2025-10-26 21:33:56 +0300

    first UI test

---
+++ hex-project-samples/src/main/java/com/company/hex/project/pages/SamplePage.java
@@ -0,0 +1,21 @@
+package com.company.hex.project.pages;
+
+import com.company.hex.ui.annotations.Element;
+import com.company.hex.ui.annotations.Page;
+import com.company.hex.ui.core.BasePage;
+import com.company.hex.ui.elements.Button;
+import com.company.hex.ui.elements.Input;
+
+@Page(url = "/sampleapp", title = "Sample Page")
+public class SamplePage extends BasePage {
+
+    @Element(name = "Юзернейм", xpath = "//input[@name='UserName']")
+    public Input username;
+
+    @Element(name = "Пароль", xpath = "//input[@name='Password']")
+    public Input password;
+
+    @Element(name = "Кнопка Логин", xpath = "//button[@id='login']")
+    public Button loginButton;
+
+}
---
+++ hex-project-samples/src/test/java/com/company/hex/project/tests/ui/UitestingplaygroundTest.java
@@ -0,0 +1,25 @@
+package com.company.hex.project.tests.ui;
+
+import com.company.hex.project.pages.SamplePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UitestingplaygroundTest {
+
+    @Test
+    @Story("Login Flow")
+    @DisplayName("Успешный вход в систему")
+    @Description("Демонстрирует простой flow авторизации на UI")
+    @Severity(SeverityLevel.CRITICAL)
+    public void testSuccessfulLogin() {
+        SamplePage page = new SamplePage();
+        page.username.fill("user");
+        page.password.fill("pwd");
+        page.loginButton.click();
+        System.out.println();
+    }
+}
